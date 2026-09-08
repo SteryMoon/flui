f9065d452b94c25141d6ca3b134199ccbf42a129
Stephanie da Siva <159732733+SteryMoon@users.noreply.github.com>
2026-09-08 20:42:06 -0300

    fix: remove import

--- app/(auth)/boas-vindas.tsx
+++ app/(auth)/boas-vindas.tsx
@@ -1,6 +1,6 @@
 import { Image } from "expo-image";
 import { router } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { View, StyleSheet } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Button } from "@/components/Button";
